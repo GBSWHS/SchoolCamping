679b9dc929b10160fcedd8ef61683331380a788f
Park Min Hyeok <opensource@pmh.codes>
2022-05-14 20:58:59 +0900

    chore: gatsby initialization

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,11 +1,14 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `SchoolCamping`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'SchoolCamping',
+    siteUrl: 'https://www.yourdomain.tld'
   },
-  plugins: ["gatsby-plugin-postcss"]
-};
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-pnpm'
+  ]
+}
 
-export default config;
+export default config
