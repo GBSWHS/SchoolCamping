bb2ad4a81e124fc74d80af1a30dd62eec21d3563
Park Min Hyeok <opensource@pmh.codes>
2022-05-14 20:12:00 +0900

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `SchoolCamping`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
+
+export default config;
