fca98de94a69ea980b02f11a8df041178eeb6dd0
Park Min Hyeok <opensource@pmh.codes>
2022-05-17 20:22:56 +0900

    fix: fix something styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { Fragment } from 'react'
 import { Helmet } from 'react-helmet'
 import Jumbotron from '../components/Jumbotron/Jumbotron'
 import ListView from '../components/ListView/ListView'
 import TopNav from '../components/TopNav/TopNav'
 
 const IndexPage = () =>
-  <motion.div>
+  <Fragment>
     <div id="top" />
     <Helmet
       title="스쿨캠핑 - 스쿨캠핑 예약을 쉽고 빠르게" />
@@ -14,6 +13,6 @@ const IndexPage = () =>
     <TopNav />
     <Jumbotron />
     <ListView />
-  </motion.div>
+  </Fragment>
 
 export default IndexPage
