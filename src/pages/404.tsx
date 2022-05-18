cca96747d712f12c84b21e4ddaf721111c4c41d2
Park Min Hyeok <opensource@pmh.codes>
2022-05-19 02:03:31 +0900

    feat: add intranet warning page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const NotFoundPage = () =>
-  <div>
-    <h1>Oops!</h1>
-    <p>page not found</p>
-  </div>
+  <div className="flex items-center justify-center w-full h-screen">
+    <div className="container">
+      <h3 className="inline-block pr-5 mb-2 text-xl font-bold border-b">Oops!</h3>
+      <p>없는 페이지 이거나 교내 인트라넷에 접속할 수 없습니다!</p>
+      <p><b>교내 Wi-Fi에 접속</b>되어 있는지 확인하세요.</p>
+    </div>
+ </div>
 
 export default NotFoundPage
