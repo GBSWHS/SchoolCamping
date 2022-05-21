4d674d2531d97370cad78dc0479ac93a20b7ae3e
Park Min Hyeok <opensource@pmh.codes>
2022-05-22 01:26:37 +0900

    feat: add offline notifications

---
+++ src/hooks/useOffline.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+
+let IS_OFFLINE = false
+
+const checkOffline = () =>
+  new Promise<boolean>((resolve) =>
+    fetch(window.location.origin)
+      .then((res) => resolve(res.status === 200))
+      .catch(() => resolve(false)))
+
+setInterval(async () => {
+  IS_OFFLINE = await checkOffline()
+}, 10 * 1000)
+
+const useOffline = () => {
+  const [isOffline, setIsOffline] = useState(IS_OFFLINE)
+
+  useEffect(() => {
+    const i = setInterval(() =>
+      setIsOffline(IS_OFFLINE))
+
+    return () =>
+      clearInterval(i)
+  })
+
+  return isOffline
+}
+
+export default useOffline
