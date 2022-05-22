f25141344d0270c1f08d460c182d24c59b5ccba8
Park Min Hyeok <opensource@pmh.codes>
2022-05-22 13:36:52 +0900

    chore: add screenshots

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,9 +26,43 @@ const config: GatsbyConfig = {
         short_name: '스쿨캠핑',
         start_url: '/',
         background_color: '#212121',
-        theme_color: '#fafafa',
+        theme_color: '#212121',
         display: 'standalone',
-        icon: 'src/images/icon.png'
+        icon: 'src/images/icon.png',
+        scope: '/',
+        orientation: 'portrait',
+        description: '경북소프트웨어고 스쿨캠핑 예약을 위한 웹 애플리케이션입니다.',
+        lang: 'ko-KR',
+        screenshots: [
+          {
+            src: 'screenshots/screenshot1.webp',
+            sizes: '1080x2171',
+            type: 'image/webp',
+            platform: 'narrow',
+            label: '홈 화면'
+          },
+          {
+            src: 'screenshots/screenshot2.webp',
+            sizes: '1080x2171',
+            type: 'image/webp',
+            platform: 'narrow',
+            label: '스쿨캠핑 예약 화면'
+          },
+          {
+            src: 'screenshots/screenshot3.webp',
+            sizes: '1080x2171',
+            type: 'image/webp',
+            platform: 'narrow',
+            label: '예약 정보 목록 및 조회 화면'
+          },
+          {
+            src: 'screenshots/screenshot4.webp',
+            sizes: '1080x2171',
+            type: 'image/webp',
+            platform: 'narrow',
+            label: '관리자 화면'
+          }
+        ]
       }
     },
     {
